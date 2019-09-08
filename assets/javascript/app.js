c862e7d09663775675d2ba1a72bbd18509557625
Grievous Angel <ronfud2000@gmail.com>
2019-09-08 17:57:46 -0400

    added more radio buttons and calculation for number correct, etc.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@ var playGame = false;
 // test function for button
 function startFunction() {
     $("#testCol").text("Shake it Up");
-    console.log("Lookee here!");
+    // console.log("Lookee here!");
 }
 
 function draw() {
@@ -65,37 +65,76 @@ function masterTimer( ){
     $("#timer").text(secStart + " round " + cnt);
   }
   cnt += 1;
-  console.log(cnt);
+  // console.log(cnt);
   if (cnt === 4) {
     secStart = 0;
-    console.log(cnt);
+    // console.log(cnt);
     $("#timer").text("0 " + " round " + cnt);
     clearInterval(myTimer);
   }
 }
 
-function myFunction() {
-  var coffee = document.forms[1];
+var answerSummary = [];
+console.log("answerSummary @ start = " + answerSummary)
+var numberCorrect = 0;
+var numberInCorrect = 0;
+var numberNoAnswer = 0;
+
+function answerOne() {
+  var question1 = document.forms[0];
   var txt = "";
   var i;
-  for (i = 0; i < coffee.length; i++) {
-    if (coffee[i].checked) {
-      txt = txt + coffee[i].value + " ";
+  for (i = 0; i < question1.length; i++) {
+    if (question1[i].checked) {
+      txt = txt + question1[i].value + " ";
+      answerSummary.push(txt);
+      console.log(" answerSummary add 1 = " + answerSummary);
     }
   }
-  document.getElementById("coffee-order").value = "You ordered a coffee with: " + txt;
+  document.getElementById("pick-1").value = "Your answer to question one is " + txt;
 }
 
-function myNextFunction() {
-  var sandwich = document.forms[2];
+function answerTwo() {
+  var question2 = document.forms[1];
   var txt = "";
-  var i;
-  for (i = 0; i < sandwich.length; i++) {
-    if (sandwich[i].checked) {
-      txt = txt + sandwich[i].value + " ";
+  var j;
+  for (j = 0; j < question2.length; j++) {
+    if (question2[j].checked) {
+      txt = txt + question2[j].value + " ";
+      answerSummary.push(txt);
+      console.log(" answerSummary add 2 = " + answerSummary);
     }
   }
-  document.getElementById("sandwich-order").value = "You ordered a " + txt + "sandwich";
+  document.getElementById("pick-2").value = "Your answer to question two is " + txt;
 }
+
+function answerThree() {
+  var question3 = document.forms[2];
+  var txt = "";
+  var k;
+  for (k = 0; k < question3.length; k++) {
+    if (question3[k].checked) {
+      txt = txt + question3[k].value + " ";
+      answerSummary.push(txt);
+      console.log(" answerSummary add 3 = " + answerSummary);
+    }
+  }
+  document.getElementById("pick-3").value = "Your answer to question three is " + txt;
+}
+
+function calcScore() {
+  for (var l = 0; l < answerSummary.length; l++) {
+    console.log("l = " + l + " answerSummary{l} = " + answerSummary[l]);
+    if (answerSummary[l] == "correct ") {
+      numberCorrect++;
+    } else if (answerSummary[l] == "incorrect ") {
+      numberInCorrect += 1;
+    }
+      numberNoAnswer = 3 - (numberCorrect + numberInCorrect);
+    }
+    console.log(numberCorrect);
+    console.log(numberInCorrect);
+    console.log(numberNoAnswer);
+  }
 
  
